96f748608cfa6efa9e0db560268d4f283ca05552
tmd <96tmdqh@gmail.com>
2024-12-18 11:22:17 +0900

    fist

--- src/main/java/com/shinhan/daengdong/member/model/repository/MemberRepositoryInterface.java
+++
@@ -1,7 +0,0 @@
-package com.shinhan.daengdong.member.model.repository;
-
-import com.shinhan.daengdong.member.dto.MemberDTO;
-
-public interface MemberRepositoryInterface {
-    MemberDTO login(MemberDTO memberDTO);
-}
--- src/main/java/com/shinhan/daengdong/member/model/service/MemberServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.shinhan.daengdong.member.model.service;
-
-import com.shinhan.daengdong.member.dto.MemberDTO;
-
-public interface MemberServiceInterface {
-    MemberDTO login(MemberDTO memberDTO);
-}
